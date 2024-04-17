1890947f848129a38a5f9f32f77f3d120ce2f24d
Nestallum <nassimlattab@gmail.com>
2024-04-17 22:17:42 +0200

    css amélioré

--- web_project/settings.py
+++ web_project/settings.py
@@ -117,11 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Définit l'URL de base pour les fichiers statiques
 STATIC_URL = '/static/'
 
+# Spécifie les répertoires où Django doit chercher les fichiers statiques supplémentaires
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
+
+# Utilisé pour collecter les fichiers statiques lors du déploiement
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
